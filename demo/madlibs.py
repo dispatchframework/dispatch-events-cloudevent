7c9dd5a63d201590ec7f6685167085578c056300
Berndt Jung <bjung@vmware.com>
2018-11-13 10:25:50 -0800

    clean up madlibs function
    Signed-off-by: Berndt Jung <bjung@vmware.com>

--- demo/madlibs.py
+++ demo/madlibs.py
@@ -5,23 +5,6 @@ import uuid
 
 import requests
 
-# event:
-#     eventtype: word.found.noun
-#     eventtypeversion: ""
-#     cloudeventsversion: "0.1"
-#     source: http://srcdog.com/madlibs
-#     eventid: 96fb5f0b-001e-0108-6dfe-da6e2806f124
-#     eventtime: "0001-01-01T00:00:00.000Z"
-#     schemaurl: ""
-#     contenttype: ""
-#     extensions:
-#       callback-url: https://srcdog.com/madlibs/event
-#     data:
-#     - 110
-#     - 117
-#     - 108
-#     - 108
-#     executedtime: 1542068285
 def handle(ctx, payload):
     event = ctx["event"]
 
@@ -47,8 +30,7 @@ def handle(ctx, payload):
         }
     }
     print("sending event to %s" % event["extensions"]["callback-url"])
-    callback = "https://srcdog.com/madlibs/event"
-    # callback = event["extensions"]["callback-url"]
+    callback = event["extensions"]["callback-url"]
     resp = requests.post(callback, headers={"Content-Type": "application/json"}, json=found_event)
     if not resp.ok:
         return {
